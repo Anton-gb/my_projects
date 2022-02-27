380cc44226425dd16570876d31bab15086fcacac
Anton-gb <aoioho@mail.ru>
2022-02-27 19:07:44 +0300

    cange files

--- remane file.py
+++ remane file.py
@@ -1,10 +1,12 @@
 import os
 
-dir_name = r''
+dir_name = r'C:\Users\User\Google Диск\Учеба\7 сем\!_Серых_тепло'
 
 os.chdir(dir_name)
 
-start_name = ''
+start_name = 'Серых_2-Сб(ТГВ)-4_'
+
+count_dwg = 1
 
 for file in os.listdir():
     file_extension = file.split('.')[-1]
@@ -18,7 +20,10 @@ for file in os.listdir():
         new_file_name = start_name + 'КП.' + file_extension
         os.rename(file, new_file_name)
     elif file_extension == 'dwg':
-        new_file_name = start_name + 'ГЧ.' + file_extension
+        new_file_name = f'{start_name}ГЧ{count_dwg}.{file_extension}'
+        if os.path.exists(new_file_name):
+            count_dwg += 1
+            new_file_name = f'{start_name}ГЧ{count_dwg}.{file_extension}'
         os.rename(file, new_file_name)
 
 for file in os.listdir():
