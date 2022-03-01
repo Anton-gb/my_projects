da4865bb1950eaacac1ff1f96aa9a7ff6818125a
Anton-gb <aoioho@mail.ru>
2022-03-01 22:19:57 +0300

    change files

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-import os
+with open('PR.txt', 'r', encoding='utf-8') as f:
+    a = f.read()
+    print(a)
+
+a = a.replace('\n', ' ')
+
+with open('new.txt', 'w', encoding='utf-8') as f:
+    f.write(a)
